997f81632e83d84fbcf4d5e250450bdd16867747
Dany <gamarra.dany@gmail.com>
2020-03-17 18:55:26 -0500

    instructor cleaning after merge

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1> {{title}} </h1>
-    <nav><a [routerLink]="['courses']">List of courses</a></nav>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
